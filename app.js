408d08f1332e453b79b647ff3a42d8e6914e111b
Arkrez <draross2019@gmail.com>
2022-03-15 16:36:54 -0700

    creating und/redo buttons

--- app.js
+++ app.js
@@ -3,9 +3,13 @@ const actionContainer = document.querySelector('.container');
 
 const eraser = document.querySelector('.eraser');
 const draw = document.querySelector('.draw');
-let undoStack = [];
-let redoStack = [];
+const undo = document.querySelector('.undo');
+const redo = document.querySelector('.redo');
+let undoStack = {};
+let redoStack = {};
 let toggle = false;
+let undoStep = -1;
+let redoStep = -1;
 
 const initBoard = function (size = 16) {
     const tileContainer = document.createElement('div');
@@ -28,15 +32,23 @@ const initBoard = function (size = 16) {
     
 }
 const changeColor = function(tile){
+    undoStep++;   
+    redoStep = -1;
+    const changedTile = [tile, toggle];
+    
     if(!toggle)
+    {
         tile.classList.add('blue');
+        undoStack[undoStep] = changedTile;
+    }
     else
     {
+        undoStack[undoStep] = changedTile;
         tile.classList.remove('blue');
     }
-        
     
 }
+
 initBoard();
 const tiles = document.querySelectorAll('.tile');
 for(const tile of tiles)
@@ -46,4 +58,33 @@ for(const tile of tiles)
 
 eraser.addEventListener('click', ()=>{toggle = true;})
 draw.addEventListener('click', ()=>{toggle = false;})
+undo.addEventListener('click', ()=>{
+    if(undoStep < 0)
+        return;
+    redoStep++;
+    
+    if(!undoStack[undoStep][1])
+        undoStack[undoStep][0].classList.remove('blue')
+    else
+        undoStack[undoStep][0].classList.add('blue');
+    redoStack[redoStep] = undoStack[undoStep];
+    delete undoStack[undoStep]
+    
+    undoStep--;
+});
+redo.addEventListener('click', ()=>{
+    if(redoStep < 0)
+        return;
+    undoStep++;
+    
+    if(redoStack[redoStep][1])
+    {
+        redoStack[redoStep][0].classList.remove('blue');
+    }
+    else
+        redoStack[redoStep][0].classList.add('blue');
+    undoStack[undoStep] = redoStack[redoStep];
+    delete redoStack[redoStep];
+    redoStep--;
+});
 
