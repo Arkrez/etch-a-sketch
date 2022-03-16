f4a060f83c9926dec49edc606f12543e585ad415
Arkrez <draross2019@gmail.com>
2022-03-15 17:00:48 -0700

    Add resizing options

--- app.js
+++ app.js
@@ -5,13 +5,24 @@ const eraser = document.querySelector('.eraser');
 const draw = document.querySelector('.draw');
 const undo = document.querySelector('.undo');
 const redo = document.querySelector('.redo');
+const re8 = document.querySelector('.re8');
+const re16 = document.querySelector('.re16');
+const re32 = document.querySelector('.re32');
+const re64 = document.querySelector('.re64');
+const re128 = document.querySelector('.re128');
 let undoStack = {};
 let redoStack = {};
 let toggle = false;
 let undoStep = -1;
 let redoStep = -1;
+let ran = false;
 
 const initBoard = function (size = 16) {
+    if(ran)
+    {
+        actionContainer.removeChild(document.querySelector('.tileContainer'));
+
+    }
     const tileContainer = document.createElement('div');
     tileContainer.classList.add('tileContainer')
     for(let i = 0; i < size; i ++)
@@ -29,7 +40,12 @@ const initBoard = function (size = 16) {
         
     }
     actionContainer.appendChild(tileContainer);
-    
+    const tiles = document.querySelectorAll('.tile');
+    for(const tile of tiles)
+    {
+        tile.addEventListener('click', ()=>changeColor(tile));
+    }
+    ran = true;
 }
 const changeColor = function(tile){
     undoStep++;   
@@ -50,11 +66,8 @@ const changeColor = function(tile){
 }
 
 initBoard();
-const tiles = document.querySelectorAll('.tile');
-for(const tile of tiles)
-{
-    tile.addEventListener('click', ()=>changeColor(tile));
-}
+
+
 
 eraser.addEventListener('click', ()=>{toggle = true;})
 draw.addEventListener('click', ()=>{toggle = false;})
@@ -73,6 +86,8 @@ undo.addEventListener('click', ()=>{
     undoStep--;
 });
 redo.addEventListener('click', ()=>{
+    if(redoStep === -1)
+        redoStack = {};
     if(redoStep < 0)
         return;
     undoStep++;
@@ -87,4 +102,20 @@ redo.addEventListener('click', ()=>{
     delete redoStack[redoStep];
     redoStep--;
 });
+
+re8.addEventListener('click', () => {
+    initBoard(8);
+})
+re16.addEventListener('click', () => {
+    initBoard(16);
+})
+re32.addEventListener('click', () => {
+    initBoard(32);
+})
+re64.addEventListener('click', () => {
+    initBoard(64);
+})
+re128.addEventListener('click', () => {
+    initBoard(128);
+})
 
